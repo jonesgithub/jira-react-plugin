a1ed070be050237242c2cbc540314513014505d7
Dmytro Kataiev <d.kataiev@gmail.com>
2018-05-25 01:02:06 +0300

    #2 use vm template for rendering

--- jira-react-plugin/src/main/java/lab/dk/servlet/ReactServlet.java
+++ jira-react-plugin/src/main/java/lab/dk/servlet/ReactServlet.java
@@ -1,22 +1,34 @@
 package lab.dk.servlet;
 
+import com.atlassian.plugin.spring.scanner.annotation.component.JiraComponent;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ReactServlet extends HttpServlet{
+@JiraComponent
+public class ReactServlet extends HttpServlet {
+
     private static final Logger log = LoggerFactory.getLogger(ReactServlet.class);
+    private final TemplateRenderer templateRenderer;
+
+    private static final String INDEX_TEMPLATE = "vm/index.vm";
+
+    @Inject
+    public ReactServlet(@ComponentImport TemplateRenderer templateRenderer) {
+        this.templateRenderer = templateRenderer;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        templateRenderer.render(INDEX_TEMPLATE, resp.getWriter());
     }
 
 }
