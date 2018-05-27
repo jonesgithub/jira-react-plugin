4f00141d5f91ac64dd9ccef6e9e73226cd11b086
Dmytro Kataiev <d.kataiev@gmail.com>
2018-05-28 00:07:10 +0300

    #4 refactor manes and mappings

--- jira-react-plugin/src/main/java/lab/dk/rest/endpoints/RootEndpoint.java
+++ jira-react-plugin/src/main/java/lab/dk/rest/endpoints/RootEndpoint.java
@@ -1,10 +1,8 @@
 package lab.dk.rest.endpoints;
 
-import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 import lab.dk.rest.model.Message;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.annotation.Resource;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -13,14 +11,12 @@ import javax.ws.rs.core.Response;
  * A resource of message.
  */
 @Slf4j
-@Path("/sec")
-//@AnonymousAllowed
+@Path("/")
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_JSON})
 public class RootEndpoint {
 
     @GET
-    //http://dk-imac:2990/jira/rest/react/latest/message
     public Response getMessage() {
         return Response.ok(Message.builder().message("Hello from protected area").build()).build();
     }
